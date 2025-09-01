3a5f29f9346946c8ff7263118896c44c9a502688
facutucci10 <tuccifacundo473@gmail.com>
2025-09-01 18:34:33 -0300

    Agregar archivos de proyecto.

---
+++ MVC-App/Data/ContactsRepository.cs
@@ -0,0 +1,65 @@
+﻿using Dapper;
+using MVC_App.Models;
+using System.Data;
+
+namespace MVC_App.Data
+{
+    public class ContactsRepository : IContactsRepository
+    {
+        private readonly IDbConnection _dbconnection;
+        public ContactsRepository(IDbConnection connection)
+        {
+            _dbconnection = connection;
+        }
+        
+        public async Task Delete(int id)
+        {
+            var consulta = @"DELETE FROM Contacts 
+                                   where Id = @id";
+
+            await _dbconnection.ExecuteAsync(consulta, new { id });
+        }
+
+        public async Task<IEnumerable<Contact>> GetAll()
+        {
+           var consulta = @"SELECT Id, FirstName, LastName, Phone, Address
+                                   FROM Contacts
+                                   ORDER BY FirstName, LastName";
+
+            return await _dbconnection.QueryAsync<Contact>(consulta, new {});
+        }
+
+        public async Task<Contact?> GetDetailsById(int id)
+        {
+            var consulta = @"SELECT Id, FirstName, LastName, Phone, Address 
+                                   FROM Contacts
+                                   WHERE Id = @Id";
+
+            return await _dbconnection.QueryFirstOrDefaultAsync<Contact>(consulta, new { Id = id });
+        }
+
+        public async Task Insert(Contact contact)
+        {
+            var consulta = @"INSERT INTO Contacts (FirstName,LastName,Phone,Address)
+                             VALUES (@name,@Lname,@Phone,@Address)";
+
+             await _dbconnection.ExecuteAsync(consulta, new {
+                name = contact.FirstName,
+                Lname = contact.LastName,
+                Phone = contact.Phone,
+                Address = contact.Address });
+        }
+
+        public async Task Update(Contact contact)
+        {
+            var consulta = @"UPDATE Contacts 
+                             SET FirstName = @name,
+                                 LastName = @Lname,
+                                 Phone = @Phone,
+                                 Address = @Address
+                             WHERE Id = @Id";
+
+             await _dbconnection.ExecuteAsync(consulta, new { Id = contact.Id, name = contact.FirstName, Lname = contact.LastName, Phone = contact.Phone, Address = contact.Address });
+        }
+    }
+}
---
+++ MVC-App/Data/IContactsRepository.cs
@@ -0,0 +1,18 @@
+﻿using MVC_App.Models;
+using System.Diagnostics.Contracts;
+
+namespace MVC_App.Data
+{
+    public interface IContactsRepository
+    {
+        Task<IEnumerable<Contact>> GetAll();
+      
+        Task<Contact?> GetDetailsById(int id);
+        
+        Task Insert(Contact contact);
+        
+        Task Update(Contact contact);
+        
+        Task Delete(int id);
+    }
+}
